0b38dec4b16908235862cf5adddd8b4c1e9b7301
root <root@DESKTOP-NGOOB5J.localdomain>
2022-02-24 01:29:29 -0500

    Los archivos que faltaron en el primer commit

---
+++ pelicanconf.py
@@ -0,0 +1,55 @@
+import os
+
+AUTHOR = 'developerstrong'
+SITENAME = 'strongtheme'
+SITEURL = 'theme-present'
+TITLE = 'Theme Strong'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Bogota'
+
+DEFAULT_LANG = 'es'
+
+DEFAULT_THEME = os.path.join(os.path.dirname(os.path.abspath(__file__)),'themes', 'theme-present')
+
+THEME = 'themes/theme-present'#DEFAULT_THEME                                         
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+READERS={"html": None}
+
+CACHE_CONTENT = False
+
+THEME_STATIC_DIR = 'themes/theme-present'
+
+THEME_STATIC_PATHS = ['static',]
+
+DISPLAY_PAGES_ON_MENU = True
+
+# Controla los templates que se renderizan y terminan en el directorio output
+DIRECT_TEMPLATES = ['index','page']
+
+ARCHIVES_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+TAGS_SAVE_AS = ''
